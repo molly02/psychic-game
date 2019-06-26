6c8c6bf81e23f8b0fd9a87df18853671aa65add5
mp02 <molly_patterson@yahoo.com>
2019-06-26 17:17:24 -0500

    fixed java link and added additional variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,35 @@
-
-<script type ="text/javascript">
-    
+var computerchoice = ["a", "b", "c"];
 
 var wins = 0;
+var losses = 0;
+
+var wins = document.getElementById("wins");
+var losses = document.getElementById("losses");
+var guessesleft = document.getElementById("guessesleft");
+var sofar = document.getElementById("sofar");
 
 
+document.onkeyup = function(event) {
 
-</script>
-
-
+    
+    var userguess = event.key;
+    var computerguess = computerchoice[Math.floor(Math.random() *computerchoice.length)];
+    
+   // if ((userguess === "a") || (userguess === "b") || (userguess === "c")) {
+        
+        if ((userguess === "a" && computerguess === "a") ||
+        (userguess === "b" && computerguess === "b") ||
+        (userguess === "c" && computerguess === "c")) {
+            wins ++;
+        }
+        else { losses ++; }
+        
+        //} 
+        // add in dispay on screen of wins, losses, what letters left
+        
+        wins.textContent = wins;
+        losses.textContent = losses;
+        guessesleft.textContent = "testing";
+        sofar.textContent = "test";
+        
+    }
